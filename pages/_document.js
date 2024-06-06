6499b4ba8a9b1bf1055ff9bb13d875e514b8b018
unknown <deepanshujain088@gmail.com>
2024-06-06 22:08:08 +0530

    Adding default meta data

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
+import logo from ""
 export default function Document() {
 
 
@@ -7,7 +7,7 @@ export default function Document() {
 		<Html lang="en">
 			<Head>
 				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-				<meta property="og:URL" content="%PUBLIC_URL%/favicon.ico" />
+				<meta property="og:URL" content="/favicon.ico" />
 
 				<meta property="og:type" content="article" />
 
@@ -15,7 +15,7 @@ export default function Document() {
 
 				<meta property="og:description" content="Welcome to the Palace on Ganges, an exquisite fusion of traditional elegance and contemporary luxury located at the heart of Varanasi’s spiritual landscape, on the famous Assi Ghat." />
 
-				<meta property="og:image" content="%PUBLIC_URL%/favicon.ico" />
+				<meta property="og:image" content="/favicon.ico" />
 			</Head>
 			<body>
 				<Main />
