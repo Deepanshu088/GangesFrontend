759ea19d6ed265c326ef43a7c7ffaeb290baef44
unknown <deepanshujain088@gmail.com>
2024-06-06 22:02:44 +0530

    Adding default meta data

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,15 @@ export default function Document() {
 		<Html lang="en">
 			<Head>
 				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+				<meta property="og:URL" content="%PUBLIC_URL%/favicon.ico" />
+
+				<meta property="og:type" content="article" />
+
+				<meta property="og:title" content="Palace on Ganges" />
+
+				<meta property="og:description" content="Welcome to the Palace on Ganges, an exquisite fusion of traditional elegance and contemporary luxury located at the heart of Varanasi’s spiritual landscape, on the famous Assi Ghat." />
+
+				<meta property="og:image" content="%PUBLIC_URL%/favicon.ico" />
 			</Head>
 			<body>
 				<Main />
